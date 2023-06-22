63b7039aaa962bd725119006a5ef188f1a14fc7a
febriann <benigiovanni3@gmail.com>
2023-06-22 11:30:29 +0700

    update login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,44 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Login</title>
+	<meta charset="UTF-8">
   <link rel="icon" type="image/x-icon" href="/img/logo.png">
-
-  <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-
-  <!-- font -->
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Zen+Dots&display=swap" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap" rel="stylesheet">
-  
-  <!-- font awesome -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <title>{{ $title }}</title>
-  <style>
-    body {
-      background-color: #0c111b;
-      font-family: 'Poppins', sans-serif;
-    }
-
-    .card {
-      width: 350px;
-      background-color: #121926;
-    }
-
-    .card:hover {
-      box-shadow: rgba(226, 226, 230, 0.2) 0px 7px 29px 0px;
-    }
-  </style>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('/css/util.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('/css/main.css') }}">
+<!--===============================================================================================-->
 </head>
-
 <body>
-  <div class="container vh-100">
-    <div class="row justify-content-center h-100">
+	
+	<div class="limiter">
       @if (session()->has('success'))
       <div style="left: 50%;
           transform: translateX(-50%); z-index: 100 !important; position: absolute;" class="w-50 top-0 z-30 alert alert-success alert-dismissible fade show" role="alert">
@@ -54,45 +46,66 @@
         {{ session('loginError') }}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
       </div>
-
       @endif
-      <div class="card my-auto">
-        <div class="card-header text-center text-white">
-          <h2>Login</h2>
-          <hr class="text-white">
-        </div>
-        <div class="card-body">
-          <form action="/login" method="post" class="text-white">
+
+		<div class="container-login100" style="background-image: url('/img/img.jpg');">
+			<div class="wrap-login100">
+				<form class="login100-form validate-form" action="/login" method="post">
+					<span class="login100-form-logo">
+            		<img src="/img/logo.png" style="width: 100px; height: 100px; padding: 15px;">
+					</span>
+					<span class="login100-form-title p-b-34 p-t-27">
+						Log in
+					</span>
             @csrf
-            <div class="form-group">
-              <label for="email">Email: </label>
-              <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" id="email" autofocus required value="{{ old('email') }}" placeholder="name@example.com">
+            <!-- Email -->
+            <div class="wrap-input100 validate-input" data-validate = "Enter Email">
+              <input class="input100 @error('email') is-invalid @enderror" type="email" name="email" id="email" autofocus required value="{{ old('email') }}" placeholder="Email">
+              <span class="focus-input100" data-placeholder="&#xf207;"></span>
               @error('email')
               <div class="invalid-feedback">
                 {{ $message }}
               </div>
               @enderror
             </div>
-            <div class="form-group">
-              <label for="password">Password: </label>
-              <input type="password" class="form-control" name="password" id="password" required placeholder="Password" required>
+
+            <!-- password -->
+            <div class="wrap-input100 validate-input" data-validate="Enter password">
+              <input class="input100" type="password" required placeholder="Password" required name="password" id="password">
+              <span class="focus-input100" data-placeholder="&#xf191;"></span>
             </div>
-            <div class="custom-control custom-checkbox mb-3">
-              <input type="checkbox" name="remember" class="custom-control-input" id="remember">
-              <label class="custom-control-label" for="remember">Remember Me</label>
+
+            <!-- cb remember me -->
+            <div class="contact100-form-checkbox">
+              <input class="input-checkbox100" id="remember" type="checkbox" name="remember">
+              <label class="label-checkbox100" for="remember">
+                Remember me
+              </label>
             </div>
-            <button type="submit" class="btn btn-primary w-100" name="submit">Login</button>
-            <hr>
-          </form>
-        </div>
-        <p class="text-center text-white" style="font-size: 16px;">Belum memiliki akun? <a href="/register" style="text-decoration: none;">Register</a></p>
-        <div class="card-footer text-center text-white">
-          <small>&copy; MovTix</small>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- login -->
+
+            <!-- btn login -->
+            <div class="container-login100-form-btn">
+              <button class="btn login100-form-btn" type="submit" name="submit">
+                Login
+              </button>
+            </div>
+
+					<div class="text-center p-t-90">
+            <p class="text-center text-white" style="font-size: 12px;">Belum memiliki akun? 
+              <a href="/register" style="text-decoration: none; font-weight: bold;">REGISTER</a>
+            </p>
+            
+            <div class="card-footer text-center text-white">
+              <small>&copy; MovTix</small>
+            </div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
 
   <script>
     $(document).ready(function() {
@@ -101,8 +114,23 @@
       });
     });
   </script>
-
+	
+<!--===============================================================================================-->
+	<script src="/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="/vendor/bootstrap/js/popper.js"></script>
+	<script src="/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="/vendor/daterangepicker/moment.min.js"></script>
+	<script src="/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="/vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="/js/main.js"></script>
 
 </body>
-
 </html>
